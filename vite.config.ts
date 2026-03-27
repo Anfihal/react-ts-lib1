31a694a7fe3445fa7e9516e0b59028e9b400a8a6
Anfi <anfisa22092004@gmail.com>
2026-03-27 14:30:16 +0300

    Deploy static site

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/react-ts-lib1/' : '/',
+  base: '/react-ts-lib1/',
   build: {
     outDir: 'dist',
     sourcemap: false,
